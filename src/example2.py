f0957b945ea7f81c84924f45af140889b4fc2fd4
pklesk <pklesk@wi.zut.edu.pl>
2023-07-23 07:49:18 +0200

    temporary example2.py

--- src/example2.py
+++ src/example2.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 if __name__ == "__main__":  
-    T = 128
+    T = 1024
     clfs = [
         AdaBoostClassifier(n_estimators=T),
         FastRealBoostBins(T=T, fit_mode="numba_jit", decision_function_mode="numba_jit"),
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         ]
     n = 1000
     np.random.seed(0) # setting some randomization seed
-    for m in [1000, 10000]:
+    for m in [100, 1000]:
         print(f"DATA SHAPE (TRAIN AND TEST): {m} x {n}")
         # generating fake random train data
         X_train = np.random.rand(m, n)
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         y_test = np.random.randint(2, size=m)
         # checking classifiers    
         for clf in clfs:
+            print(f"  CLF: {clf} ...")
             t1_fit = time.time()      
             clf.fit(X_train, y_train)
             t2_fit = time.time()
@@ -32,7 +33,6 @@ if __name__ == "__main__":
             t2_predict_train = time.time()            
             t1_predict_test = time.time()
             acc_test = clf.score(X_train, y_train)
-            t2_predict_test = time.time()
-            print(f"  CLF: {clf}")            
+            t2_predict_test = time.time()                        
             print(f"    ACCs -> TRAIN {clf.score(X_train, y_train)}, TEST: {clf.score(X_test, y_test)}")            
             print(f"    TIMES [s] -> FIT: {t2_fit - t1_fit}, PREDICT (TRAIN): {t2_predict_train - t1_predict_train}, PREDICT (TEST): {t2_predict_test - t1_predict_test}")            
