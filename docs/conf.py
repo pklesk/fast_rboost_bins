4daa9ff7e4a9a079912733f58f6824d47fae8f4a
pklesk <pklesk@wi.zut.edu.pl>
2023-08-09 19:50:23 +0200

    more docstrings added

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,8 @@ copyright = '2023, Przemysław Klęsk'
 author = 'Przemysław Klęsk'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+version = '0.9.0'
+release = '0.9.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -59,3 +60,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'version_dropdown': True
+}
